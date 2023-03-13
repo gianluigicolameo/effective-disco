0f2b2f4fd33a92920d9c40d8f002084029d2943f
gianluigicolameo <g.colameo@enway.it>
2023-03-13 17:18:14 +0100

    commit swagger ui

--- src/main/java/com/enway/FileMangementFilesApplication.java
+++ src/main/java/com/enway/FileMangementFilesApplication.java
@@ -2,12 +2,37 @@ package com.enway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FileMangementFilesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileMangementFilesApplication.class, args);
+	}
+	
+	@Bean
+	public Docket api() {
+
+		return new Docket(DocumentationType.OAS_30).select().apis( RequestHandlerSelectors.any())
+				.paths( PathSelectors.any()).build();
+	}
+
+	private ApiInfo apiInfo() {
+
+		return new ApiInfoBuilder().title("RestAPI").description("Employee Service Exposed Operations").version("1.0")
+				.build();
 	}
 
 }
